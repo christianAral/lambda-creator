09f9175fe78b6b0a1f272253f9692e6053c9d74d
christianAral <=>
2025-05-22 22:06:01 -0400

    testing function

--- test.py
+++ test.py
@@ -1,10 +1,16 @@
+import tomllib, json
 import sys
 sys.path.append('src')
 import src.lambda_handler
 
 if __name__ == '__main__':
     # Test the lambda_handler function
-    event = {}
+    with open('manifest.toml','rb') as f:
+        manifest = tomllib.load(f)
+
+    event = {
+        "body": json.dumps(manifest)
+    }
     context = {}
     response = src.lambda_handler.lambda_handler(event, context)
     print(response)
