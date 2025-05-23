a18d50e51eeba21d6cdb17c57443b4b9179fd789
christianAral <=>
2025-05-22 22:05:04 -0400

    remove template files

--- src/examples/boto3_examples.py
+++
@@ -1,7 +0,0 @@
-import boto3
-import json
-
-def secrets_manager_retreival():
-    client = boto3.client('secretsmanager')
-    resp = client.get_secret_value(SecretId='dummy/secret')
-    return json.loads(resp['SecretString'])
--- src/examples/requests_examples.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-def requests_get_example():
-    url = 'https://1.1.1.1'
-    resp = requests.get(url)
-    return resp.status_code
